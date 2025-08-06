79a520286af0c69a6170175d2f4588a93dd8e991
Moritz Rinow <m.rinow@topmotive.eu>
2025-08-06 11:32:22 +0200

    neo-tree, treesitter, colorscheme

--- init.lua
+++ init.lua
@@ -78,7 +78,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add your plugins here
+    -- import your plugins
+    { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.

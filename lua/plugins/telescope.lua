0a2031e84b888ce50223dcb771d1af486871e67f
Moritz Rinow <m.rinow@topmotive.eu>
2025-08-09 23:39:38 +0200

    replace neo-tree with oil, devicons with mini.icons

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,6 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
-    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
     require('telescope').setup {
